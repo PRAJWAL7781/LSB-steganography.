6e459593c0f6546c0f61cc717726bf663c916f6a
PRAJWAL7781 <164867777+PRAJWAL7781@users.noreply.github.com>
2025-11-17 23:36:53 +0530

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     char ch;
+//     double dnum;
+//     string str;
+
+//     cout << "Enter the num ";
+//     cin >> num;
+//     cout << "Enter the char ";
+//     cin >> ch;
+//     cout << "Enter the dnum ";
+//     cin >> dnum;
+//     getchar();
+//     cout << "Enter the string ";
+//     //cin>>str;
+//   getline(cin,str);
+//     cout << num << "  " << ch << "  " << dnum << " " <<  str << endl;
+// }
